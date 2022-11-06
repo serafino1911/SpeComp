09f13d655c81896e7abd520622f0e873a810fe44
serafino1911 <72070650+serafino1911@users.noreply.github.com>
2022-11-06 11:49:34 +0100

    Base upgrade, test function

--- src/tools/comarator.py
+++ src/tools/comarator.py
@@ -11,29 +11,23 @@ def load_data(path):
         y = [float(line[1]) for line in data]
     return x, y
 
+def clear_y(y_test):
+    k = np.min(y_test)
+    y_test = [y-k for y in y_test]
+    return y_test
 
-def compare_timeseries_correlation(x_1 : list, y_1 : list, x_2 : list, y_2 : list):
-    # differnte lenght
+def select_intervall(x_1, y_1, x_2, y_2):
     if len(x_1) != len(y_1) or len(x_2) != len(y_2):
         return 0
     # find same intervall
     x_min = max(x_1[0], x_2[0])
     x_max = min(x_1[-1], x_2[-1])
-    # find index
-    i_min = 0
-    i_max = 0
-    for i in range(len(x_1)):
-        if x_1[i] >= x_min:
-            i_min = i
-            break
+    i_min = next((i for i in range(len(x_1)) if x_1[i] >= x_min), 0)
     for i in range(len(x_2)):
         if x_2[i] >= x_min:
             i_min = i
             break
-    for i in range(len(x_1)):
-        if x_1[i] >= x_max:
-            i_max = i
-            break
+    i_max = next((i for i in range(len(x_1)) if x_1[i] >= x_max), 0)
     for i in range(len(x_2)):
         if x_2[i] >= x_max:
             i_max = i
@@ -43,55 +37,52 @@ def compare_timeseries_correlation(x_1 : list, y_1 : list, x_2 : list, y_2 : lis
     y_1 = y_1[i_min:i_max]
     x_2 = x_2[i_min:i_max]
     y_2 = y_2[i_min:i_max]
-    #resample to same length
-    y_1 = np.linspace(y_1[0], y_1[-1], len(y_2))
+    return x_1, y_1, x_2, y_2
+
+def compare_timeseries_integral(x_1 : list, y_1 : list, x_2 : list, y_2 : list):
+    x_1, y_1, x_2, y_2 = select_intervall(x_1, y_1, x_2, y_2)
+    y_1 = clear_y(y_1)
+    y_2 = clear_y(y_2)
 
 
-    corr = np.corrcoef(y_1, y_2)[0, 1]
-    return corr
-
-
-def discrete_correlation(x_1 : list, y_1 : list, x_2 : list, y_2 : list):
-    # differnte lenght
-    if len(x_1) != len(y_1) or len(x_2) != len(y_2):
-        return 0
-    # find same intervall
-    x_min = max(x_1[0], x_2[0])
-    x_max = min(x_1[-1], x_2[-1])
-    # find index
-    i_min = 0
-    i_max = 0
-    for i in range(len(x_1)):
-        if x_1[i] >= x_min:
-            i_min = i
-            break
-    for i in range(len(x_2)):
-        if x_2[i] >= x_min:
-            i_min = i
-            break
-    for i in range(len(x_1)):
-        if x_1[i] >= x_max:
-            i_max = i
-            break
-    for i in range(len(x_2)):
-        if x_2[i] >= x_max:
-            i_max = i
-            break
-    # cut
-    x_1 = x_1[i_min:i_max]
-    y_1 = y_1[i_min:i_max]
-    x_2 = x_2[i_min:i_max]
-    y_2 = y_2[i_min:i_max]
-    #interpolate to same x values
-    #y_1 = np.interp(x_2, x_1, y_1)
-    #scipy interpolation
     f = scipy.interpolate.interp1d(x_1, y_1, fill_value="extrapolate")
     y_1 = f(x_2)
 
-    #y_2 = np.interp(x_1, x_2, y_2)
+    corr = np.correlate(y_1, y_2, mode='same')
+    return np.trapz(corr)
+
+
+def compare_timeseries_correlation(x_1 : list, y_1 : list, x_2 : list, y_2 : list):
+    x_1, y_1, x_2, y_2 = select_intervall(x_1, y_1, x_2, y_2)
     #resample to same length
-    #y_1 = np.linspace(y_1[0], y_1[-1], len(y_2))
-    #calculate correlation
+    y_1 = clear_y(y_1)
+    y_2 = clear_y(y_2)
+    y_1 = np.linspace(y_1[0], y_1[-1], len(y_2))
+    return np.corrcoef(y_1, y_2)[0, 1]
+
+def ultra_compare_timeseries_correlation(x_1 : list, y_1 : list, x_2 : list, y_2 : list):
+    x_1, y_1, x_2, y_2 = select_intervall(x_1, y_1, x_2, y_2)
+    #resample to same length
+    y_1 = clear_y(y_1)
+    y_2 = clear_y(y_2)
+    min_delta_x1 = min(x_1[i+1]-x_1[i] for i in range(len(x_1)-1))
+    min_delta_x2 = min(x_2[i+1]-x_2[i] for i in range(len(x_2)-1))
+    min_delta_x = min(min_delta_x1, min_delta_x2)
+    new_x_1 = np.arange(x_1[0], x_1[-1], min_delta_x/2)
+    new_x_2 = np.arange(x_2[0], x_2[-1], min_delta_x/2)
+    y_1 = np.interp(new_x_1, x_1, y_1)
+    y_2 = np.interp(new_x_2, x_2, y_2)
+
+    f = scipy.interpolate.interp1d(new_x_1, y_1, fill_value="extrapolate")
+    y_1 = f(new_x_2)
+    return np.corrcoef(y_1, y_2)[0, 1]
+
+def discrete_correlation(x_1 : list, y_1 : list, x_2 : list, y_2 : list):
+    x_1, y_1, x_2, y_2 = select_intervall(x_1, y_1, x_2, y_2)
+    y_1 = clear_y(y_1)
+    y_2 = clear_y(y_2)
+    f = scipy.interpolate.interp1d(x_1, y_1, fill_value="extrapolate")
+    y_1 = f(x_2)
     corr = np.corrcoef(y_1, y_2)[0,1]
     return np.abs(corr)
 
@@ -101,8 +92,10 @@ def compare(path1, file_list):
     x_u, y_u = load_data(path1)
     for file in file_list:
         x2, y2 = load_data(file)
-
-        corr = discrete_correlation(x_u, y_u, x2, y2)
+        corr = ultra_compare_timeseries_correlation(x_u, y_u, x2, y2)
+        #corr = discrete_correlation(x_u, y_u, x2, y2)
+        #corr = compare_timeseries_correlation(x_u, y_u, x2, y2)
+        #corr = compare_timeseries_integral(x_u, y_u, x2, y2)
         file_results.append((file, corr))
     #sort by correlation
     file_results.sort(key=lambda x: x[1], reverse=True)
@@ -110,13 +103,13 @@ def compare(path1, file_list):
 
 
 BASE_DIR = 'data\\DB'
-unknown = 'data\\Unknown\\EXE.txt'
+unknown = 'data\\Unknown\\fb3.txt'
 file_list = []
 for root, dirs, files in os.walk(BASE_DIR):
     file_list.extend(os.path.join(root, file) for file in files if file.endswith('.txt'))
 
 results = compare(unknown,file_list)
-print(results)
+print(results[:5])
 #take first and plot it
 import matplotlib.pyplot as plt
 x_u, y_u = load_data(unknown)
